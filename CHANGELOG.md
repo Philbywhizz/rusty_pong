16df6ea4551a916c9f6de9df2c064940eadf84d3
Phil Howlett <phil.g.howlett@gmail.com>
2021-04-14 22:13:05 +1000

    move rect/mesh items out of draw method and into MainState new

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -8,6 +8,9 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/).
 ### Added
 - CHANGELOG.md file to track and record changes from the original project
 
+### Changed
+- Refactored the creation of mesh objects outside of the main loop
+
 ## 0.1.0 2020-JUL-17
 
 The original [rusty_pong](https://github.com/TanTanDev/rusty_pong) repository release
--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,7 @@ const RACKET_HEIGHT_HALF: f32 = RACKET_HEIGHT * 0.5;
 const BALL_SIZE: f32 = 30.0;
 const BALL_SIZE_HALF: f32 = BALL_SIZE * 0.5;
 const PLAYER_SPEED: f32 = 600.0;
-const BALL_SPEED: f32 = 500.0;
+const BALL_SPEED: f32 = 300.0;
 
 fn move_racket(pos: &mut na::Point2<f32>, keycode: KeyCode, y_dir: f32, ctx: &mut Context) {
     let dt = ggez::timer::delta(ctx).as_secs_f32();
@@ -51,24 +51,61 @@ struct MainState {
     ball_vel: na::Vector2<f32>,
     player_1_score: i32,
     player_2_score: i32,
+    racket_mesh: graphics::Mesh,
+    ball_mesh: graphics::Mesh,
+    middle_mesh: graphics::Mesh,
 }
 
 impl MainState {
-    pub fn new(ctx: &mut Context) -> Self {
+    pub fn new(ctx: &mut Context) -> GameResult<MainState> {
         let (screen_w, screen_h) = graphics::drawable_size(ctx);
         let (screen_w_half, screen_h_half) = (screen_w * 0.5, screen_h * 0.5);
 
         let mut ball_vel = na::Vector2::new(0.0, 0.0);
         randomize_vec(&mut ball_vel, BALL_SPEED, BALL_SPEED);
 
-        MainState {
+        let racket_rect = graphics::Rect::new(
+            -RACKET_WIDTH_HALF,
+            -RACKET_HEIGHT_HALF,
+            RACKET_WIDTH,
+            RACKET_HEIGHT,
+        );
+
+        let racket_mesh = graphics::Mesh::new_rectangle(
+            ctx,
+            graphics::DrawMode::fill(),
+            racket_rect,
+            graphics::WHITE,
+        )?;
+
+        let ball_rect = graphics::Rect::new(-BALL_SIZE_HALF, -BALL_SIZE_HALF, BALL_SIZE, BALL_SIZE);
+
+        let ball_mesh = graphics::Mesh::new_rectangle(
+            ctx,
+            graphics::DrawMode::fill(),
+            ball_rect,
+            graphics::WHITE,
+        )?;
+
+        let middle_rect = graphics::Rect::new(-MIDDLE_LINE_W * 0.5, 0.0, MIDDLE_LINE_W, screen_h);
+        let middle_mesh = graphics::Mesh::new_rectangle(
+            ctx,
+            graphics::DrawMode::fill(),
+            middle_rect,
+            graphics::WHITE,
+        )?;
+
+        Ok(MainState {
             player_1_pos: na::Point2::new(RACKET_WIDTH_HALF + PADDING, screen_h_half),
             player_2_pos: na::Point2::new(screen_w - RACKET_WIDTH_HALF - PADDING, screen_h_half),
             ball_pos: na::Point2::new(screen_w_half, screen_h_half),
             ball_vel,
             player_1_score: 0,
             player_2_score: 0,
-        }
+            racket_mesh,
+            ball_mesh,
+            middle_mesh,
+        })
     }
 }
 
@@ -111,81 +148,61 @@ impl event::EventHandler for MainState {
             && self.ball_pos.y - BALL_SIZE_HALF < self.player_1_pos.y + RACKET_HEIGHT_HALF
             && self.ball_pos.y + BALL_SIZE_HALF > self.player_1_pos.y - RACKET_HEIGHT_HALF;
 
+        // If the ball overlaps player_1 then change direction
         if intersects_player_1 {
             self.ball_vel.x = self.ball_vel.x.abs();
         }
+
         let intersects_player_2 = self.ball_pos.x - BALL_SIZE_HALF
             < self.player_2_pos.x + RACKET_WIDTH_HALF
             && self.ball_pos.x + BALL_SIZE_HALF > self.player_2_pos.x - RACKET_WIDTH_HALF
             && self.ball_pos.y - BALL_SIZE_HALF < self.player_2_pos.y + RACKET_HEIGHT_HALF
             && self.ball_pos.y + BALL_SIZE_HALF > self.player_2_pos.y - RACKET_HEIGHT_HALF;
 
+        // If the ball overlaps player_1 then change direction
         if intersects_player_2 {
             self.ball_vel.x = -self.ball_vel.x.abs();
         }
         Ok(())
     }
 
+    // draw related stuff goes here
     fn draw(&mut self, ctx: &mut Context) -> GameResult {
+        // Clear the screen
         graphics::clear(ctx, graphics::BLACK);
-
-        let racket_rect = graphics::Rect::new(
-            -RACKET_WIDTH_HALF,
-            -RACKET_HEIGHT_HALF,
-            RACKET_WIDTH,
-            RACKET_HEIGHT,
-        );
-        let racket_mesh = graphics::Mesh::new_rectangle(
-            ctx,
-            graphics::DrawMode::fill(),
-            racket_rect,
-            graphics::WHITE,
-        )?;
-
-        let ball_rect = graphics::Rect::new(-BALL_SIZE_HALF, -BALL_SIZE_HALF, BALL_SIZE, BALL_SIZE);
-        let ball_mesh = graphics::Mesh::new_rectangle(
-            ctx,
-            graphics::DrawMode::fill(),
-            ball_rect,
-            graphics::WHITE,
-        )?;
-
-        let screen_h = graphics::drawable_size(ctx).1;
-        let middle_rect = graphics::Rect::new(-MIDDLE_LINE_W * 0.5, 0.0, MIDDLE_LINE_W, screen_h);
-        let middle_mesh = graphics::Mesh::new_rectangle(
-            ctx,
-            graphics::DrawMode::fill(),
-            middle_rect,
-            graphics::WHITE,
-        )?;
 
         let mut draw_param = graphics::DrawParam::default();
 
+        // draw the middle net
         let screen_middle_x = graphics::drawable_size(ctx).0 * 0.5;
         draw_param.dest = [screen_middle_x, 0.0].into();
-        graphics::draw(ctx, &middle_mesh, draw_param)?;
+        graphics::draw(ctx, &self.middle_mesh, draw_param)?;
 
+        // draw racket 1
         draw_param.dest = self.player_1_pos.into();
-        graphics::draw(ctx, &racket_mesh, draw_param)?;
+        graphics::draw(ctx, &self.racket_mesh, draw_param)?;
 
+        // draw racket 2
         draw_param.dest = self.player_2_pos.into();
-        graphics::draw(ctx, &racket_mesh, draw_param)?;
+        graphics::draw(ctx, &self.racket_mesh, draw_param)?;
 
+        // draw ball
         draw_param.dest = self.ball_pos.into();
-        graphics::draw(ctx, &ball_mesh, draw_param)?;
+        graphics::draw(ctx, &self.ball_mesh, draw_param)?;
 
+        // calculate score text
         let score_text = graphics::Text::new(format!(
-            "{}         {}",
+            "{}      {}",
             self.player_1_score, self.player_2_score
         ));
         let screen_w = graphics::drawable_size(ctx).0;
         let screen_w_half = screen_w * 0.5;
-
         let mut score_pos = na::Point2::new(screen_w_half, 40.0);
         let (score_text_w, score_text_h) = score_text.dimensions(ctx);
         score_pos -= na::Vector2::new(score_text_w as f32 * 0.5, score_text_h as f32 * 0.5);
-        draw_param.dest = score_pos.into();
 
+        // draw score
+        draw_param.dest = score_pos.into();
         graphics::draw(ctx, &score_text, draw_param)?;
 
         graphics::present(ctx)?;
@@ -195,8 +212,9 @@ impl event::EventHandler for MainState {
 
 fn main() -> GameResult {
     let cb = ggez::ContextBuilder::new("rusty_pong", "Philbywhizz");
-    let (mut ctx, mut event_loop) = cb.build()?;
+    let (ctx, event_loop) = &mut cb.build()?;
+
     graphics::set_window_title(&ctx, "Rusty Pong");
-    let mut state = MainState::new(&mut ctx);
-    event::run(&mut ctx, &mut event_loop, &mut state)
+    let state = &mut MainState::new(ctx)?;
+    event::run(ctx, event_loop, state)
 }
